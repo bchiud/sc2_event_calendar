737acf7d8ade8d9e613debba65e543e826a7d5a7
bchiud <brady.chiu@gmail.com>
2016-04-13 21:06:17 -0700

    update

--- tl_sc2_cal_scraper.R
+++ tl_sc2_cal_scraper.R
@@ -1,35 +1,67 @@
 setwd("/Users/bradychiu/Dropbox (Uber Technologies)/R/sc2_event_calendar")
 
 library(lubridate)
+library(stringr)
+library(XML)
 
-# gets html src code for today and tomorrow's calendar
+# gets html src code for this month's calendar
 get_html_src<-function(time=Sys.time()){
-  tl_link<-paste(
-    "http://www.teamliquid.net/calendar/?view=week&year="
+  tl_link<<-paste(
+    "http://www.teamliquid.net/calendar/?view=month&year="
     ,format(time,"%Y")
     ,"&month="
     ,format(time,"%m")
     ,"&day="
     ,format(time,"%d")
+    ,"&team=&league=&game=1"
     ,sep=""
-    )
+  )
   html_src<<-htmlParse(readLines(tl_link))
 }
 get_html_src()
 
+# day : div class="ev-feed" data-day="4"         <td class="evc-l">
+day<-xpathSApply(
+  doc=html_src
+  ,path='//td[@class="evc-l"]'
+  ,fun=xmlValue
+)
+
+temp_df<-data.frame(raw_string=strsplit(day[3],"\n")[[1]])
+temp_df$tabs<-sapply(temp_df$raw_string,function(x) str_count(x,"\t"))
+temp_df
+
+test_vector<-c("asdf\t\tasdf","asdfasfd","asdf\t\t\tasdf")
+grepl("\t",test_vector)
+grep("\t",test_vector)
+length(strsplit(test_vector,"\t")[[1]])
+length(strsplit(test_vector,"\t")[[2]])
+length(strsplit(test_vector,"\t")[[3]])
+str_count(test_vector[1],"\t")
+str_count(test_vector[2],"\t")
+str_count(test_vector[3],"\t")
 
 
 # event : <span data-event-id="29477">GSL Code S</span>
-xpathSApply(html_src, path='//span[@data-event-id="29477"]', fun=xmlValue)
+event<-xpathSApply(
+  doc=html_src
+  ,path='//div[@class="ev-ctrl"]'
+  ,fun=xmlValue
+)
 
 # stage : <div class="ev-stage">Round of 16 - Group B</div>
-stage <- xpathSApply(html_src, path='//span[@class="ev-stage"]', fun=xmlValue)
+stage<-xpathSApply(
+  doc=html_src
+  ,path='//div[@class="ev-stage"]'
+  ,fun=xmlValue
+)
 
 # time : <span class="ev-timer">09:30</span>
-time <- xpathSApply(html_src, path='//span[@class="ev-timer"]', fun=xmlValue)
-getNodeSet
+time<-xpathSApply(
+  doc=html_src
+  ,path='//span[@class="ev-timer"]'
+  ,fun=xmlValue
+)
+time<-time[time!=""]
 
-
-
-
-xpathApply(htmlParse(tl_link_tomorrow),'//span[@class="ev-timer"]',xmlValue)
+# get blogger urls with XML:<br>library(RCurl)<br>library(XML)<br>script <- getURL("www.r-bloggers.com")<br>doc <- htmlParse(script)<br>li <- getNodeSet(doc, "//ul[@class='xoxo blogroll']//a")<br>urls <- sapply(li, xmlGetAttr, "href")<br>
